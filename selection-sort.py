b60b875dc7642e30fdaae76dda97300c03f67336
Shruti Kiran <shrutik@Kirans-Mac-Mini.local>
2017-09-25 07:44:56 +0530

    optimize selection sort

--- selection-sort.py
+++ selection-sort.py
@@ -7,11 +7,12 @@ def selectionSort(arr):
         for j in range(i + 1, l):
             if (arr[minIx] > arr[j]):
                 minIx = j
-        print('swapping indicies [' + str(i) + ', ' + str(minIx) + ']')
-        temp = arr[i]
-        arr[i] = arr[minIx]
-        arr[minIx] = temp
-        print(str(arr))
+        if (minIx != j):
+            print('swapping indicies [' + str(i) + ', ' + str(minIx) + ']')
+            temp = arr[i]
+            arr[i] = arr[minIx]
+            arr[minIx] = temp
+            print(str(arr))
 
 if __name__ == '__main__':
     arr = [10, 5, 2, 18, 92, 12, -1, 48]
