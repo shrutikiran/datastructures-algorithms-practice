286a4dc42fda3ff8c9499666bf2564601056fcd0
kirn <kirn@cisco.com>
2018-03-30 23:25:35 +0530

    single linked list: finding the intersecting node

--- src/main/java/linked_list/single/SLLHelper.java
+++ src/main/java/linked_list/single/SLLHelper.java
@@ -138,4 +138,33 @@ public class SLLHelper {
         return null;
     }
 
+    public static SLLNode findIntersectingNode(SLLNode list1, SLLNode list2) {
+        int c1 = count(list1), c2 = count(list2);
+
+        int     delta = (c1 > c2) ? (c1 - c2) : (c2 - c1);
+        SLLNode ptr1 = (c1 > c2) ? list1 : list2;
+        SLLNode ptr2 = (ptr1 == list1) ? list2 : list1;
+
+        while (delta-- > 0) {
+            ptr1 = ptr1.next;
+        }
+
+        while (ptr1 != ptr2) {
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+
+        return (ptr1 == ptr2) ? ptr1 : null;
+    }
+
+    public static int count(SLLNode head) {
+        int c = 0;
+
+        while (head != null) {
+            head = head.next;
+            c++;
+        }
+
+        return c;
+    }
 }
--- src/main/java/linked_list/single/SLLTests.java
+++ src/main/java/linked_list/single/SLLTests.java
@@ -45,18 +45,46 @@ public class SLLTests {
         return head;
     }
 
+    private static SLLNode createIntersectingList(SLLNode list1) {
+        SLLNode head;
+
+        head = new SLLNode(0);
+
+        SLLNode ptr;
+        int v = -1;
+
+        for (ptr = head; ptr.next == null; ptr = ptr.next) {
+            if (v >= 0) {
+                break;
+            }
+            ptr.next = new SLLNode(v++);
+        }
+
+        ptr.next = SLLHelper.findMiddleNode(list1);
+
+        return head;
+    }
+
     public static void main(String[] args) {
         SLLNode head, ptr;
 
         head = createList();
 
-        System.out.println("Iterative Traversal");
-        SLLHelper.traverse_Iterative(head, (node) -> System.out.print(node.value + " "));
-        System.out.println();
+        {
+            System.out.println();
+            System.out.println("Iterative Traversal");
+            SLLHelper.traverse_Iterative(head, (node) -> System.out.print(node.value + " "));
+            System.out.println();
+            System.out.println();
+        }
 
-        System.out.println("Recursive Traversal");
-        SLLHelper.traverse_Recursive(head, (node) -> System.out.print(node.value + " "));
-        System.out.println();
+        {
+            System.out.println();
+            System.out.println("Recursive Traversal");
+            SLLHelper.traverse_Recursive(head, (node) -> System.out.print(node.value + " "));
+            System.out.println();
+            System.out.println();
+        }
 
         {
             System.out.println("Reverse (Recursive)");
@@ -64,36 +92,44 @@ public class SLLTests {
             temp = SLLHelper.reverse_Recursive(temp, null);
             SLLHelper.traverse_Iterative(temp, (node) -> System.out.print(node.value + " "));
             System.out.println();
+            System.out.println();
         }
 
         {
+            System.out.println();
             System.out.println("Reverse (Iterative)");
             SLLNode temp = createList();
             temp = SLLHelper.reverse_Iterative(temp);
             SLLHelper.traverse_Iterative(temp, (node) -> System.out.print(node.value + " "));
             System.out.println();
+            System.out.println();
         }
 
         {
+            System.out.println();
             head = createList();
-            System.out.print("Middle node (Even Number)");
+            System.out.print("Middle node (Even Number): ");
             ptr = SLLHelper.findMiddleNode(head);
             System.out.println(ptr == null ? "null" : ptr.value);
 
-            System.out.print("Middle node (Odd Number)");
+            System.out.print("Middle node (Odd Number): ");
             ptr = SLLHelper.findMiddleNode(head.next.next.next);
             System.out.println(ptr == null ? "null" : ptr.value);
+            System.out.println();
         }
 
         {
+            System.out.println();
             int position = 2;
             head = createList();
             System.out.print("nth (" + position + ") node from end: ");
             SLLNode nthNode = SLLHelper.nthNodeFromLast_Iterative(head, position);
             System.out.println((nthNode != null) ? nthNode.value : "null");
+            System.out.println();
         }
 
         {
+            System.out.println();
             head = createList();
             System.out.println("Linear list; has loop = " + SLLHelper.hasLoop(head));
 
@@ -101,6 +137,18 @@ public class SLLTests {
             System.out.println("Circular list; has loop = " + SLLHelper.hasLoop(head));
             SLLNode loopStartNode = SLLHelper.findLoopStartNode(head);
             System.out.println("Loop start node: " + ((loopStartNode != null) ? loopStartNode.value : "null"));
+            System.out.println();
+        }
+
+        {
+            System.out.println();
+            SLLNode list1 = createList();
+            SLLNode list2 = createIntersectingList(list1);
+
+            System.out.println("Finding the intersecting Node");
+            SLLNode intersectingNode = SLLHelper.findIntersectingNode(list1, list2);
+            System.out.println("Intersecting node: " + ((intersectingNode != null) ? intersectingNode.value : "null"));
+            System.out.println();
         }
     }
 }
